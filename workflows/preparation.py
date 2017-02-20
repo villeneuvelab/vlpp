59bc3b35074c13c468c9b73f7b7c5d6e672f267e
Christophe Bedetti <christophe.bedetti@gmail.com>
2017-02-20 09:36:45 -0500

    Take care of T1 mgz file in preparation

--- workflows/preparation.py
+++ workflows/preparation.py
@@ -19,28 +19,33 @@ class Preparation(WorkflowManager):
 
     def connect(self, main_wf):
         petfiles = main_wf.get_node('petfiles')
+        fssource = main_wf.get_node('fssource')
 
         main_wf.connect([
-            (petfiles, self._wf, [
-                ('frametimes', 'sortframes.frametimes'),
-                #('petframes', 'convert.petframes'),
-                ]),
+            (petfiles, self._wf, [('frametimes', 'sortframes.frametimes')]),
+            (fssource, self._wf, [('T1', 'mriconvert.in_file')]),
             ])
 
 
     def _UCD(self):
         from interfaces.sortframes import SortFrames
-        #from interfaces.convert import Convert
+        from nipype.interfaces.freesurfer.preprocess import MRIConvert
         from nipype.pipeline.engine import Node, Workflow
 
+        def ext(in_file):
+            from nipype.utils.filemanip import split_filename
+            _, base, ext = split_filename(in_file)
+            return ext
+
         sortframes = Node(SortFrames(origin='UCD'), 'sortframes')
-        #TODO Check nifti or dcm format
-        #convert = Node(Convert(image_format=config['image_format'], 'convert')
+
+        mriconvert = Node(MRIConvert(out_type='nii'), 'mriconvert')
 
         wf = Workflow(self.name)
+
         wf.add_nodes([
             sortframes,
-            #convert,
+            mriconvert,
             ])
 
         return wf
