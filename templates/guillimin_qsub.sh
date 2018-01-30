247873c1497a30cde2209f880046e0ae7883d103
Christophe Bedetti <christophe.bedetti@gmail.com>
2018-01-30 14:07:55 -0500

    update qsub script for qa

--- templates/guillimin_qsub.sh
+++ templates/guillimin_qsub.sh
@@ -11,39 +11,38 @@ module purge
 module use /sf1/project/yai-974-aa/local/modulefiles
 module load VilleneuveLab
 
-
 {% if dev %}
+
 module load vlpp/dev
 source activate vlpp-dev
-{% endif %}
 
+{% endif %}
 
 cd ${PBS_O_WORKDIR}
 mkdir -p {{participant}}
 cd {{participant}}
 
-
 {% if qa %}
+
 vlpp-qa.nf -resume
 
 {% else %}
+
 vlpp.nf --pet {{pet}} --freesurfer {{freesurfer}} --participant {{participant}} -c ../code/config.cfg -resume
 
 {% endif %}
-
 {% if not dev %}
+
 # Removing work directory if pipeline success
 EXITCODE=$?
 if [[ $EXITCODE -eq 0 ]]
 then
     rm -Rf work/
+{% if qa %}
+    tar zcvf ../qa.tar.gz assets/ data/*js data/*jpg *.html
+{% endif %}
 fi
 
 
 {% endif %}
 cd ${PBS_O_WORKDIR}
-
-
-{% if qa %}
-tar zcvf qa.tar.gz qa/assets/ qa/data/*js qa/data/*jpg qa/*.html
-{% endif %}
