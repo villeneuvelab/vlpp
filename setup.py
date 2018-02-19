5c2ea2d36c4fe0fc3a6d4644b8b4467219b9cb4f
Christophe Bedetti <christophe.bedetti@gmail.com>
2018-02-19 15:08:09 -0500

    v1.2.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 DISTNAME = "vlpp"
 DESCRIPTION = description
-VERSION = "1.2.0"
+VERSION = "1.2.1"
 AUTHOR = "Christophe Bedetti"
 AUTHOR_EMAIL = "christophe.bedetti@umontreal.ca"
 #URL = "https://github.com/"
