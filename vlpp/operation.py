7ecee27dfb517ed963a282fa1d91734c854720c7
Christophe Bedetti <christophe.bedetti@umontreal.ca>
2018-06-12 10:25:24 -0400

    Add volumes column in tsv output

--- vlpp/operation.py
+++ vlpp/operation.py
@@ -51,25 +51,25 @@ def computeSuvr(input, mask, output):
 class ImgStatsFromAtlas(object):
 
     USER_LABELS = {
-        "pibindex_Lfrontal": [1003,1012,1014,1018,1019,1020,1027,1028,1032],
-        "pibindex_Ltemporal": [1009,1015,1030],
-        "pibindex_Lparietal": [1008,1025,1029,1031],
-        "pibindex_Lcingulate": [1002,1023,1010,1026],
-        "pibindex_Rfrontal": [2003,2012,2014,2018,2019,2020,2027,2028,2032],
-        "pibindex_Rtemporal": [2009,2015,2030],
-        "pibindex_Rparietal": [2008,2025,2029,2031],
-        "pibindex_Rcingulate": [2002,2023,2010,2026],
-        "pibindex_Ltemporal_noIT": [1015,1030],
-        "pibindex_Rtemporal_noIT": [2015,2030],
-        "PIBINDEX_noIT": [1003,1012,1014,1018,1019,1020,1027,1028,1032,1008,
+        "amyloid_index_Lfrontal": [1003,1012,1014,1018,1019,1020,1027,1028,1032],
+        "amyloid_index_Ltemporal": [1009,1015,1030],
+        "amyloid_index_Lparietal": [1008,1025,1029,1031],
+        "amyloid_index_Lcingulate": [1002,1023,1010,1026],
+        "amyloid_index_Rfrontal": [2003,2012,2014,2018,2019,2020,2027,2028,2032],
+        "amyloid_index_Rtemporal": [2009,2015,2030],
+        "amyloid_index_Rparietal": [2008,2025,2029,2031],
+        "amyloid_index_Rcingulate": [2002,2023,2010,2026],
+        "amyloid_index_Ltemporal_noIT": [1015,1030],
+        "amyloid_index_Rtemporal_noIT": [2015,2030],
+        "amyloid_index_noIT": [1003,1012,1014,1018,1019,1020,1027,1028,1032,1008,
             1025,1029,1031,1002,1023,1010,1026,2003,2012,2014,2018,2019,2020,
             2027,2028,2032,2008,2025,2029,2031,2002,2023,2010,2026,1015,1030,
             2015,2030],
-        "PIBINDEX": [1003,1012,1014,1018,1019,1020,1027,1028,1032,1008,1025,
+        "amyloid_index": [1003,1012,1014,1018,1019,1020,1027,1028,1032,1008,1025,
             1029,1031,1002,1023,1010,1026,2003,2012,2014,2018,2019,2020,2027,
             2028,2032,2008,2025,2029,2031,2002,2023,2010,2026,1015,1030,2015,
             2030,2009,1009],
-        "MediaPFC": [1014,2014,1028,2028,1026,2026,1002,2002]
+        "amyloid_index_MediaPFC": [1014,2014,1028,2028,1026,2026,1002,2002]
         }
 
     def __init__(self, img, atlas, output):
@@ -77,52 +77,57 @@ class ImgStatsFromAtlas(object):
         self.atlas = atlas
 
         self.outputDefault = "default.stats"
-        self.outputSpecial = "special.stats"
         self.output = output
 
     def computeDefault(self):
         cmd = "mri_segstats --ctab-default --excludeid 0 --i {0} --seg {1} --sum {2}"
         run_shell(cmd.format(self.img, self.atlas, self.outputDefault))
+        names = [
+                "Index", "SegId", "NVoxels", "Volume_mm3",
+                "StructName", "Mean", "StdDev", "Min", "Max", "Range",
+                ]
+        self.data = pandas.read_csv(
+                self.outputDefault, delim_whitespace=True,
+                comment="#", names=names)
 
     def computeSpecial(self):
-        volData = nib.load(self.img).get_data()
+        volInfo = nib.load(self.img)
+        volData = volInfo.get_data()
+        voxelVolume = reduce(lambda x, y: x*y, volInfo.header.get_zooms())
         atlasData = nib.load(self.atlas).get_data()
+        lastIndex = self.data.Index.tail(1).values[0]
 
-        stats = [['StructName', 'mean', 'std', 'min', 'max', 'range']]
-        for structName, values in self.USER_LABELS.items():
+        stats = [['NVoxels', 'StructName', 'mean', 'std', 'min', 'max', 'range']]
+        rslRows = []
+        for structName, values in sorted(self.USER_LABELS.items()):
+            lastIndex += 1
+
             mask = np.zeros_like(atlasData)
             for value in values:
                 mask[atlasData==value] = 1
 
             data = np.ma.masked_where(mask==0, volData, copy=True)
 
-            stats.append([
-                structName,
-                float("{0:.4f}".format(np.nanmean(data))),
-                float("{0:.4f}".format(np.nanstd(data))),
-                float("{0:.4f}".format(np.nanmin(data))),
-                float("{0:.4f}".format(np.nanmax(data))),
-                float("{0:.4f}".format(np.nanmax(data)-np.nanmin(data))),
-                ])
+            rslRows.append({
+                "Index": lastIndex,
+                "SegId": np.nan,
+                "NVoxels": mask.sum(),
+                "Volume_mm3": mask.sum() * voxelVolume,
+                "StructName": structName,
+                "Mean": float("{0:.4f}".format(np.nanmean(data))),
+                "StdDev": float("{0:.4f}".format(np.nanstd(data))),
+                "Min": float("{0:.4f}".format(np.nanmin(data))),
+                "Max": float("{0:.4f}".format(np.nanmax(data))),
+                "Range": float("{0:.4f}".format(np.nanmax(data)-np.nanmin(data))),
+                })
 
-        with open(self.outputSpecial, 'wt') as csvfile:
-            w = csv.writer(csvfile)#, delimiter=',', quotechar='"')
-            w.writerows(stats)
-
-    def mergeResults(self):
-        specData = pandas.read_csv(self.outputSpecial)
-        data = pandas.read_csv(
-                self.outputDefault, delim_whitespace=True, comment="#",
-                usecols=[4, 5, 6, 7, 8, 9], names=specData.columns.values)
-        mergeData = specData.append(data)
-        mergeData.to_csv(self.output, sep="\t", index=False)
+        self.data = self.data.append(rslRows, ignore_index=True)
 
     def compute(self):
         self.computeDefault()
         self.computeSpecial()
-        self.mergeResults()
+        self.data.to_csv(self.output, sep="\t", index=False)
         run_shell("rm {}".format(self.outputDefault))
-        run_shell("rm {}".format(self.outputSpecial))
 
 
 class CentiloidStats(object):
